10015cc1cd7c27ae5a1da3a02ece70fed11f2462
bsibdev <71753160+bsibdev@users.noreply.github.com>
2024-05-28 21:52:14 -0400

    assign variables to buttons and add more style

--- script.js
+++ script.js
@@ -18,3 +18,15 @@ const negButton = document.querySelector("#neg-button");
 const zeroButton = document.querySelector("#zero-button");
 const decimalButton = document.querySelector("#decimal-button");
 const operateButton = document.querySelector("#operate-button");
+
+//document.addEventListener('click',)
+
+let num1 = 3;
+let num2 = 5;
+
+function add(num1,num2) {
+let sum = num1 + num2;
+return sum;
+}
+
+console.log(add(num1,num2));
